c82718c71d738e1c72fafa7d9b07d51af0023f52
Ricardo Balbi <rbalbi@gmail.com>
2020-08-24 16:05:28 -0300

    chore(monorepo): setup monorepo
    add server, mobile and eslint & prettier configuration.

---
+++ packages/shared/eslint-config/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/standard',
+    'prettier/react'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true
+    },
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    semi: [2, 'always'], // Added by rbalbi
+    'space-before-function-paren': ['error', 'never'], // Added by rbalbi
+    '@typescript-eslint/explicit-function-return-type': 'off' // Added by rbalbi
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {}
+    },
+    react: {
+      version: 'detect'
+    }
+  }
+};
